6f96514b27e812dfe21f82c44cfe28f8dfc90f65
Pete Smith <roysvork@gmail.com>
2014-10-28 15:18:29 +0000

    Added support for mocking and fixed issues that prevented the lib from working after minification.

--- D.I.Y.js/D.I.Y.debug.js
+++ D.I.Y.js/D.I.Y.debug.js
@@ -1,5 +1,4 @@
 ﻿(function (diy) {
-
     // Enums
     var mappingTypes = {
         Singleton: 0,
@@ -10,6 +9,42 @@
     // Private members
     var containers = {};
 
+    var replaceMembers = function (mockType) {
+        if (typeof mockType == "function") {
+            var container = {
+                func: mockType
+            };
+
+            spyOn(container, "func");
+            return container.func;
+        }
+
+        if (typeof mockType == "object") {
+            var objs = Object.getOwnPropertyNames(mockType);
+            for (var i in objs) {
+                spyOn(mockType, objs[i]);
+            }
+        }
+
+        return mockType;
+    }
+    
+    var extend = function (out) {
+        out = out || {};
+
+        for (var i = 1; i < arguments.length; i++) {
+            if (!arguments[i])
+                continue;
+
+            for (var key in arguments[i]) {
+                if (arguments[i].hasOwnProperty(key))
+                    out[key] = arguments[i][key];
+            }
+        }
+
+        return out;
+    };
+
     // Static methods
     diy.getAllContainers = function () {
         return containers;
@@ -17,10 +52,9 @@
 
     // Define container
     diy.Container = diy.Container || {};
-    
+
     // Container constructor
     diy.Container.New = function (name) {
-
         // Argument exceptions
         if (name) {
             if (containers["name"]) {
@@ -30,42 +64,44 @@
                 };
             }
         }
-        
+
         // Private fields
         var mappings = {};
 
         // Private methods
-        var bind = function(key, constructor) {
+        var bind = function (key, constructor) {
             mappings[key] = { type: mappingTypes.Instance, data: constructor };
         };
 
-        var bindSingleton = function(key, instance) {
+        var bindSingleton = function (key, instance) {
             mappings[key] = { type: mappingTypes.Singleton, data: instance };
         };
 
-        var bindMethod = function(key, method) {
+        var bindMethod = function (key, method) {
             mappings[key] = { type: mappingTypes.Method, data: method };
         };
 
         // Public members
         var container = {};
         
+        // Properties
+        container.Name = name;
+
         // Methods
         container.bind = bind;
         container.bindSingleton = bindSingleton;
         container.bindMethod = bindMethod;
 
-        container.reset = function() {
+        container.reset = function () {
             mappings = {};
         };
 
-        container.getDependency = function(key) {
+        container.getDependency = function (key) {
             var mapping = mappings[key];
-            
             if (!mapping) {
                 throw {
                     name: "DIYError",
-                    message: "A binding was not found for argument '" + key + "' in the specified container"                  
+                    message: "A binding was not found for argument '" + key + "' in the specified container"
                 };
             }
 
@@ -73,30 +109,91 @@
                 case mappingTypes.Singleton:
                     return mapping.data;
                 case mappingTypes.Instance:
-                    return mapping.data.DIY();
+                    return mapping.data.DIY(name);
                 case mappingTypes.Method:
                     return mapping.data();
             }
-            
+
             throw {
                 name: "DIYError",
-                message: "One or more bindings more incorrectly configured"         
+                message: "One or more bindings more incorrectly configured"
             };
         };
-    
+
+        container.getMock = function (key) {
+            var mapping = mappings[key];
+
+            if (!mapping) {
+                throw {
+                    name: "DIYError",
+                    message: "A binding was not found for argument '" + key + "' in the specified container"
+                };
+            }
+
+            var mockType;
+            switch (mapping.type) {
+                case mappingTypes.Singleton:
+                    mockType = mapping.data;
+                    break;
+                case mappingTypes.Instance:
+                    mockType = mockDependency(mapping.data);
+                    break;
+                case mappingTypes.Method:
+                    mockType = mapping.data();
+                    break;
+            }
+
+            return replaceMembers(mockType);
+        };
+
         // Add container to global collection
         if (name) {
             containers[name] = container;
         } else {
             containers.default = container;
         }
-        
+
         return container;
     };
-    
-    // Extend function prototype for D.I in constructor functions
-    Function.prototype.DIY = function (containerName) {
 
+    var mockDependency = function (dependency) {
+        if (dependency.isSpy) {
+            dependency = dependency.originalValue;
+        }
+
+        var args = extractParamNames(dependency);
+        var len = args.length;
+
+        var params = [];
+
+        if (args.length > 1 || args[0].length > 0) {
+            for (var i = params.length; i < len; i++) {
+                params.push({});
+            }
+        }
+        var context = {};
+        context.protected = {};
+        context.base = null;
+        context.derivesFrom = function (constructor) {
+            context.base = mockDependency(constructor);
+        };
+        context.dependsOn = function () {
+            var args = [].slice.call(arguments);
+            for (var index in args) {
+                argument = args[index];
+                context[argument] = {};
+            }
+        }
+
+        if (typeof dependency.DerivesFrom == "function") {
+            context.base = mockDependency(dependency.DerivesFrom);
+        }
+
+        // Invoke the constructor with correct dependencies
+        return dependency.apply(context, params);
+    };
+
+    var getContainer = function (containerName) {
         var container;
         if (containerName) {
             container = containers[containerName];
@@ -117,6 +214,22 @@
             }
         }
 
+        return container;
+    };
+
+    var extractParamNames = function (obj) {
+        var string = obj.toString();
+        var start = string.indexOf("(") + 1;
+        var end = string.indexOf(")");
+
+        var args = string.substring(start, end).split(",");
+        return args;
+    }
+
+    // Extend function prototype for D.I in constructor functions
+    Function.prototype.DIY = function (containerName, protectedMembers, extraParams) {
+        var container = getContainer(containerName);
+
         // Make sure we can handle Jasmine spies
         var obj = this;
         if (obj.isSpy) {
@@ -124,24 +237,106 @@
         }
 
         // Decode the function header and extract parameters
-        var string = obj.toString();
-        var start = string.indexOf("(") + 1;
-        var end = string.indexOf(")");
-
-        var args = string.substring(start, end).split(",");
+        var args = extractParamNames(obj);
         var len = args.length;
 
-        var params = [];
+        var params = extraParams || [];
 
         // Determine all required dependencies
         if (args.length > 1 || args[0].length > 0) {
-            for (var i = 0; i < len; i++) {
+            for (var i = params.length; i < len; i++) {
                 params.push(container.getDependency(args[i].trim()));
             }
         }
 
+        // New style that will allow minification
+        var context = {};
+        context.protected = protectedMembers || {};
+        context.base = null;
+        context.derivesFrom = function (constructor) {
+            context.base = constructor.DIY(containerName, context.protected);
+            context = extend(context, context.protected);
+        };
+        
+        context.dependsOn = function () {
+            var args = [].slice.call(arguments);
+            for (var index in args) {
+                argument = args[index];
+                context[argument] = container.getDependency(argument.trim());
+            }
+        }
+
+        if (typeof this.DerivesFrom == "function") {
+            context.base = this.DerivesFrom.DIY(containerName, context.protected);
+        }
+
         // Invoke the constructor with correct dependencies
-        return this.apply({}, params);
+        return this.apply(context, params);
     };
 
+    Function.prototype.AutoMock = function (containerName, extraParams) {
+        var container = getContainer(containerName);
+
+        var obj = this;
+        if (obj.isSpy) {
+            obj = obj.originalValue;
+        }
+
+        var args = extractParamNames(obj);
+        var len = args.length;
+
+        var params = extraParams || [];
+
+        var mock = {
+            dependencies: {}
+        };
+
+        if (args.length > 1 || args[0].length > 0) {
+            for (var i = params.length; i < len; i++) {
+                var name = args[i].trim();
+                var dependencyMock = container.getMock(name);
+
+                mock.dependencies[name] = dependencyMock;
+                params.push(dependencyMock);
+            }
+        }
+
+        // New style that will allow minification
+        var base;
+        var context = {};
+        context.protected = {};
+        context.base = null;
+        context.derivesFrom = function (constructor) {
+            base = constructor.AutoMock(containerName);
+            context.base = base.obj;
+        };
+        context.dependsOn = function () {
+            var args = [].slice.call(arguments);
+            for (var index in args) {
+                var name = args[index].trim();
+                var dependencyMock = container.getMock(name);
+
+                mock.dependencies[name] = dependencyMock;
+                context[name] = dependencyMock;
+            }
+        }
+
+        if (typeof this.DerivesFrom == "function") {
+            base = this.DerivesFrom.AutoMock(containerName);
+            context.base = base.obj;
+        }
+
+        // Invoke the constructor with correct dependencies
+        mock.obj = this.apply(context, params);
+        if (base) {
+            mock.base = base;
+        }
+
+        return mock;
+    };
+
+    Function.prototype.Mock = function (containerName) {
+        var mockType = mockDependency(this);
+        return replaceMembers(mockType);
+    }
 })(window.DIY = window.DIY || {});
